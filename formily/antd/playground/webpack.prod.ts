6124a00acd6d045d00fab612075b3e53caec7da4
zhouyantao <zhouyantao@karrytech.com>
2023-05-29 17:25:44 +0800

    feat: webpack增加打包压缩

--- formily/antd/playground/webpack.prod.ts
+++ formily/antd/playground/webpack.prod.ts
@@ -2,6 +2,7 @@ import baseConfig from './webpack.base'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import MonacoPlugin from 'monaco-editor-webpack-plugin'
+import CompressionPlugin from 'compression-webpack-plugin'
 import path from 'path'
 
 const createPages = (pages) => {
@@ -32,6 +33,21 @@ export default {
     ]),
     new MonacoPlugin({
       languages: ['json'],
+    }),
+    new CompressionPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+    new CompressionPlugin({
+      filename: '[path].br[query]',
+      algorithm: 'brotliCompress',
+      test: /\.(js|css|html|svg)$/,
+      compressionOptions: { level: 11 },
+      threshold: 10240,
+      minRatio: 0.8,
     }),
   ],
   optimization: {
